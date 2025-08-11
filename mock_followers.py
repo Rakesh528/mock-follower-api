f6be90e26a0509a627b2ed7be3cbca191e2cdbba
Along-God <127703906+Rakesh528@users.noreply.github.com>
2025-08-11 06:00:43 +0530

    Add files via upload

--- mock_followers.py
+++ mock_followers.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def followers(username):
     count = random.choice([1000, 5000, 10000, 50000, 1000000])
     data = {
-        "username": username,
-        "follower_count": count,
+        "username": "testing1234969",
+        "follower_count": 5000,
         "last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "source": "Mock API - Educational Testing Only"
     }
